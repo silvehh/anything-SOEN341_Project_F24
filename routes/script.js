d388061b1be0002a1e2073bfb4be4348ef994cb3
Pogut <p.hejazi2014@gmail.com>
2024-09-25 22:53:12 -0400

    uploaded the new script.js to work with the file system

---
+++ routes/script.js
@@ -0,0 +1,34 @@
+function redirectToLogin(userType) {
+    if (userType === 'student') {
+        window.location.href = 'student-login.html';
+    } else if (userType === 'teacher') {
+        window.location.href = 'teacher-login.html';
+    }
+}
+
+document.addEventListener('mousemove', function (e) {
+    const container = document.querySelector('.container');
+    const rect = container.getBoundingClientRect();
+    const offsetX = (e.clientX - (rect.left + rect.width / 2)) / 15;
+    const offsetY = (e.clientY - (rect.top + rect.height / 2)) / 15;
+    container.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+});
+function loginWithGoogle() {
+    window.location.href = 'https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=your-redirect-url&response_type=code&client_id=your-client-id&scope=email%20profile&access_type=online';
+}
+
+document.addEventListener('mousemove', function (e) {
+    const container = document.querySelector('.login-container');
+    const rect = container.getBoundingClientRect();
+    const offsetX = (e.clientX - (rect.left + rect.width / 2)) / 15;
+    const offsetY = (e.clientY - (rect.top + rect.height / 2)) / 15;
+    container.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+});
+
+document.addEventListener('mousemove', function (e) {
+    const container = document.querySelector('.login-container');
+    const rect = container.getBoundingClientRect();
+    const offsetX = (e.clientX - (rect.left + rect.width / 2)) / 15;
+    const offsetY = (e.clientY - (rect.top + rect.height / 2)) / 15;
+    container.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+});
