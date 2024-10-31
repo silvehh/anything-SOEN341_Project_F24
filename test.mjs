46e747ab6fef010ad093010e055708f7209efd5a
HaniHan <slhane0503@gmail.com>
2024-10-31 04:19:30 -0400

    Deleting some Tests

--- test.mjs
+++ test.mjs
@@ -1,6 +1,9 @@
 import request from 'supertest'
 import app from './app.js'
 
+const request = require('supertest'); // Import request from Supertest
+const app = require('./app'); // Adjust the path to your app file
+
 test("Hello world!", (t) =>{
     t.pass();
 })
@@ -9,13 +12,11 @@ test("GET / - should load the homepage", async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
   });
-  
-const request = require('supertest'); // Import request from Supertest
-const app = require('./app'); // Adjust the path to your app file
 
-describe('GET / - Homepage', () => {
+
+/**  describe('GET / - Homepage', () => {
 test("GET / - should load the homepage", async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
   });
-});
+}); */
