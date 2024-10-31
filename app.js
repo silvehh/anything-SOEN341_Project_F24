54729d91fdaa8b0601e67c96dcdbcbf805dbe09e
Amro Atique <amromatique@gmail.com>
2024-10-31 01:33:18 -0400

    Successfully added pushing new credentials to CSV with a nasty sign up pop up

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
@@ -11,6 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/routes', express.static(path.join(__dirname, 'routes')));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -18,6 +20,22 @@ app.get('/', (req, res) => {
 
 app.get('/signup', (req, res) => {
   res.render('signup');
+});
+
+app.post('/signup-process', (req, res) => {
+  const { name, email, password, role } = req.body;
+  const filePath = path.join(__dirname, 'data', role === 'teacher' ? 'teachers.csv' : 'students.csv');
+
+  const data = `${name},${email},${password}\n`;
+
+  fs.appendFile(filePath, data, (err) => {
+      if (err) {
+          console.error('Error writing to CSV file', err);
+          return res.status(500).send('Error signing up, please try again.');
+      }
+      console.log(`${role} signed up successfully`);
+      res.send(`${role.charAt(0).toUpperCase() + role.slice(1)} signed up successfully!`);
+  });
 });
 
 app.get('/student-login', (req, res) => {
