fe0733674a52941efac42f336c0acfd0fe16b0b6
Frankies1002 <frankiesvat@gmail.com>
2024-11-14 14:57:33 -0500

    Added Comments

--- test/valid.js
+++ test/valid.js
@@ -1,3 +1,4 @@
+//Export function to test 
 export function rating(input){
     return input >= 1 && input <= 5;
 }
--- test/valid.test.js
+++ test/valid.test.js
@@ -1,21 +1,27 @@
 import { describe, test, it, expect } from 'vitest';
 import { rating } from './valid.js'
 
-
+//Create test 
 describe('rating', () => {
 
+    //Test #1 testing with value of 0 and it should return false
     it('Should return true if value between 1 and 5', () => {
        
         expect(rating(0)).toBe(false);
     })
+
+    //Test #2 testing with valie of 1 and it should return true
     it('Should return true if value between 1 and 5', () => {
        
         expect(rating(1)).toBe(true);
     })
+
+    //Test #3 testing with value of 2 and it should return true
     it('Should return true if value between 1 and 5', () => {
        
         expect(rating(2)).toBe(true);
     })
+    //Test #4 testing with value of 3 and it should return true
     it('Should return true if value between 1 and 5', () => {
        
         expect(rating(3)).toBe(true);
