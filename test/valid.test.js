e67a6dbf08e879f7c875bc0662de23e17f57ddf4
Frankies1002 <frankiesvat@gmail.com>
2024-11-14 15:19:31 -0500

    Made switch from Vitest to mocha

--- test/valid.test.js
+++ test/valid.test.js
@@ -1,6 +1,6 @@
 
-import { describe, test, it, expect } from 'vitest';
-import { rating } from './valid.js'
+import { expect } from 'chai';
+import { rating } from '../valid.js'
 
 //Create test case
 describe('rating', () => {
@@ -8,55 +8,56 @@ describe('rating', () => {
     //Test #1 testing with value of 0 and it should return false
     it('Should return true if value between 1 and 5', () => {
        
-        expect(rating(0)).toBe(false);
+        expect(rating(0)).to.equal(false);
     })
 
     //Test #2 testing with valie of 1 and it should return true
     it('Should return true if value between 1 and 5', () => {
        
-        expect(rating(1)).toBe(true);
+        expect(rating(1)).to.equal(true);
     })
 
     //Test #3 testing with value of 2 and it should return true
     it('Should return true if value between 1 and 5', () => {
        
-        expect(rating(2)).toBe(true);
+        expect(rating(2)).to.equal(true);
     })
 
     //Test #4 testing with value of 3 and it should return true
     it('Should return true if value between 1 and 5', () => {
        
-        expect(rating(3)).toBe(true);
+        expect(rating(3)).to.equal(true);
     })
 
     //Test #5 testing with value of 4 and it should return true
     it('Should return true if value between 1 and 5', () => {
        
-        expect(rating(4)).toBe(true);
+        expect(rating(4)).to.equal(true);
     })
 
     //Test #6 testing with value of 5 and it should return true
     it('Should return true if value between 1 and 5', () => {
        
-        expect(rating(5)).toBe(true);
+        expect(rating(5)).to.equal(true);
     })
 
     //Test #7 testing with value of 6 and it should return false 
     it('Should return true if value between 1 and 5', () => {
        
-        expect(rating(6)).toBe(false);
+        expect(rating(6)).to.equal(false);
     })
 
     //Test #8 testing with 100 and it should return false
     it('Should return true if value between 1 and 5', () => {
        
-        expect(rating(100)).toBe(false);
+        expect(rating(100)).to.equal(false);
     })
 
     //Test #9 testing with -1 and it should return false
     it('Should return true if value between 1 and 5', () => {
        
-        expect(rating(-1)).toBe(false);
+        expect(rating(-1)).to.equal(false);
     })
 
 })
+    
