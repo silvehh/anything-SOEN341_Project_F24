3ed9e33c5af0d72461b48c594d56560b4c822857
FerIron <ferhaanhawas@yahoo.ca>
2024-11-28 00:07:27 -0500

    comments

--- test/isAlphanumeric.mjs
+++ test/isAlphanumeric.mjs
@@ -1,3 +1,4 @@
+//exporting alphanumeral function
 export function isAlphanumeric(input) {
     const alphanumericRegex = /^[a-zA-Z0-9 ]+$/; // Check if inputs is only letter, numbers or space
     return alphanumericRegex.test(input);
