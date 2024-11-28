58f4ff12f1cb4e8c65ba936681ac47d28b48f7a3
Frankies1002 <frankiesvat@gmail.com>
2024-11-28 01:07:56 -0500

    comments

--- test/sentenceCheck.mjs
+++ test/sentenceCheck.mjs
@@ -1,7 +1,8 @@
 export function containsOffensiveWords(input, offensiveWords) {
 
-    if (typeof input !== 'string') return false;
+    if (typeof input !== 'string') //Check if the inputs is a string
+        return false;
     const words = input.toLowerCase().split(/\s+/);
-    return words.some(word => offensiveWords.includes(word));
-    
+        return words.some(word => offensiveWords.includes(word)); //check if the word is in the senetence
+
 }
